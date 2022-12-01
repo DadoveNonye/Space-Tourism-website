324ddb8d87d48739e53d6123ec4355d4191efcc4
Nonye <dadovenoye@gmail.com>
2022-12-01 05:50:52 +0100

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,14 +8,17 @@ function Home() {
     <div className="HomeBody">
       <Navbar />
       <div className="HomeBodyText">
-        <h3>SO, YOU WANT TO TRAVEL TO</h3>
-        <h1 className="homeheader">SPACE</h1>
-        <p>
-          Let's face it; if you want to go to the space, you might as well
-          genuinely go to the outter space and not hover kind of on the edge of
-          it. Well sit back and relax because we will give you a truly out of
-          this world experience!
-        </p>
+        <div className="homeText">
+          <h3>SO, YOU WANT TO TRAVEL TO</h3>
+          <h1 className="homeheader">SPACE</h1>
+          <p>
+            Let's face it; if you want to go to the space, you might as well
+            genuinely go to the outter space and not hover kind of on the edge
+            of it. Well sit back and relax because we will give you a truly out
+            of this world experience!
+          </p>
+        </div>
+
         <div className="explore">
           <p>EXPLORE</p>
         </div>
